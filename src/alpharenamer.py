5815089ca4fb809b8591dbda72102d95f39d9e6b
gozaliivan <gozaliivan@7f3d32f9-e71d-42ef-87b3-a642ebc53577>
2011-11-27 17:19:11 +0000

    Calls renamed. Still thinking of how to rename all other variables and handle dot operators.
    git-svn-id: https://bullseye.cs.berkeley.edu/i-svn/devdatta/2011/extensions/ivan/ExtensionAnalyzer@428 7f3d32f9-e71d-42ef-87b3-a642ebc53577

--- src/alpharenamer.py
+++ src/alpharenamer.py
@@ -9,6 +9,8 @@ import jsparser
 import sexp
 
 DEBUGGING = False
+SEPARATOR = '_R_'
+FUNCSEP   = '_F_'
 
 def debugprint(str=""):
     global DEBUGGING
@@ -64,6 +66,8 @@ class Frame(object):
     parentFrame  - reference to its parent frame
     function     - reference to the function node in the AST
     varDecls     - a list that contains Variable objects in the scope
+    callList     - a list of all unique function calls in the scope
+    identifierList - a list of all identifiers to be renamed in the frame
     childrenFrames - a list that contains children Frame objects 
     """
     new_id = 0
@@ -78,14 +82,14 @@ class Frame(object):
         self.function = function
         
         self.varDecls = list()
+        self.callList = list()
+        self.identifierList = list()
         self.childrenFrames = list()
         
         # Inserting renamed variables to frame's varDecls
         if function is not None and function.params != []:
             for var_name in function.params:
                 self.varDecls.append(FormalParam(var_name, function.params))
-        
-        
     
     def add_variable(self, var):
         """Adds a Variable object to the current frame's varDecls
@@ -93,6 +97,12 @@ class Frame(object):
         var - A Variable object
         """
         self.varDecls.append(var)
+    
+    def add_call(self, call):
+        self.callList.append(call)
+    
+    def add_identifier(self, identifier):
+        self.identifierList.append(identifier)
     
     def lookup_variable(self, identifier):
         """Attempts to find a variable in the current frame. If 
@@ -115,7 +125,25 @@ class Frame(object):
                 frame_ptr = frame_ptr.parentFrame
         
         return None
+    
+    def lookup_function(self, function_name):
+        function_to_find = get_real_function_name(function_name)
+        frame_ptr = self
+        while frame_ptr is not None:
+            function_list = [get_real_function_name(frame.name) for frame in frame_ptr.childrenFrames]
 
+            if function_to_find in function_list:
+                return frame_ptr.childrenFrames[function_list.index(function_to_find)].name
+            else:
+                frame_ptr = frame_ptr.parentFrame
+        
+        return None
+    
+    def set_name(self, new_name):
+        self.name = new_name
+        if self.function:
+            self.function.name = new_name
+        
     def get_global_frame(self):
         """ Call this method on any frame to get the top-most frame,
         a.k.a. the global frame.
@@ -134,6 +162,7 @@ class Frame(object):
         while frame_ptr.parentFrame is not None:
             frame_ptr = frame_ptr.parentFrame
         return frame_ptr
+    
     def __str__(self):
         result = ""
         
@@ -141,6 +170,8 @@ class Frame(object):
         result += self.name + "(" + str(self.id) + ")" + ", "
         result += (self.parentFrame.name if self.parentFrame is not None else "None") + ", "
         result += str(self.varDecls) + ", "
+        result += str(self.callList) + ", "
+        result += str(self.identifierList) + ", "
         
         result += "[\n\t"
         for frame in self.childrenFrames:
@@ -157,23 +188,48 @@ def get_real_identifier(identifier):
     If the identifier passed in is already real, then return itself
     
     The format of a renamed identifier is:
-        <REAL_IDENTIFIER>_<FRAME_ID>
+        <REAL_IDENTIFIER><SEPARATOR><FRAME_ID>
+    Where separator is defined in this module as a global variable
     
     Examples:
-        a_0
-        chrome_1
+        a_R_0
+        chrome_R_1
     
-    >>> get_real_identifier('a_0')
+    >>> get_real_identifier('a_R_0')
     'a'
-    >>> get_real_identifier('chrome_27')
+    >>> get_real_identifier('chrome_R_27')
     'chrome'
     >>> get_real_identifier('foo')
     'foo'
     """
-    if "_" in identifier:
-        return identifier[:identifier.index("_")]
+    global SEPARATOR
+    if SEPARATOR in identifier:
+        return identifier[:identifier.index(SEPARATOR)]
     return identifier
 
+def get_real_function_name(name):
+    global FUNCSEP
+    if FUNCSEP in name:
+        return name[:name.index(FUNCSEP)]
+    return name
+
+def isrenamed(name):
+    return FUNCSEP in name or SEPARATOR in name
+
+class Call(object):
+    """ Represents a call statement in JavaScript """
+    def __init__(self, node):
+        self.name = node[0].value
+        self.node = node
+    
+    def set_name(self, new_name):
+        self.name = new_name
+        self.node[0].value = new_name
+    
+    def __str__(self):
+        return str(self.name)
+    __repr__ = __str__
+    
 class Variable(object):
     """
     Represents a generic variable container. It has an identifier and a
@@ -242,6 +298,23 @@ class FormalParam(Variable):
     def set_value(self, new_value):
         pass
 
+class Identifier(object):
+    """ Represents an identifier in JavaScript. An Identifier object
+    knows its own name, its AST node and in which frame its located.
+    """
+    
+    def __init__(self, node):
+        self.name = node.value
+        self.node = node
+    
+    def __str__(self):
+        return self.name
+    __repr__ = __str__
+    
+    def set_name(self, new_name):
+        self.name = new_name
+        self.node.value = new_name
+
 def create_frames(ast):
     # Closure trick in Python 2.7
     class Namespace(object): pass
@@ -250,18 +323,13 @@ def create_frames(ast):
     ns.current_frame = ns.frame
     ns.current_level = 1
     
-    def is_variable_node(node):
-        if getattr(node, "type", None) == "VAR":
+    def is_node_type(node, type):
+        if getattr(node, "type", None) == type:
             return True
         return False
     
-    def is_function_node(node):
-        if getattr(node, "type", None) == "FUNCTION":
-            return True
-        return False
-    
-    def get_variable(node, level):
-        if is_function_node(node):
+    def create_frames_helper(node, level):
+        if is_node_type(node, "FUNCTION"):
             if ns.current_level > level:
                 level_difference = ns.current_level - level
                 for _ in range(level_difference // 2):
@@ -284,7 +352,7 @@ def create_frames(ast):
             
             ns.current_level += 2
             
-        if is_variable_node(node):
+        if is_node_type(node, "VAR"):
             if ns.current_level > level:
                 level_difference = ns.current_level - level
                 for _ in range(level_difference // 2):
@@ -295,8 +363,34 @@ def create_frames(ast):
             var = VarAssign(node)
             # debugprint("Before add variable: ", ns.current_frame)
             ns.current_frame.add_variable(var)
-    
-    traverse_AST_level(ast, get_variable)
+        
+        # TODO: Put identifiers in a list inside the corresponding frame.
+        if is_node_type(node, "IDENTIFIER"):
+            if ns.current_level > level:
+                level_difference = ns.current_level - level
+                for _ in range(level_difference // 2):
+                    ns.current_frame = ns.current_frame.parentFrame
+                
+                ns.current_level = level
+            # TODO: Need to filter out renamed identifiers.
+            # Question: Do we put identifiers here during frame creation, 
+            # or after we rename everything?
+            identifier = Identifier(node)
+            ns.current_frame.add_identifier(identifier)
+        
+        if is_node_type(node, "CALL"):
+            if ns.current_level > level:
+                level_difference = ns.current_level - level
+                for _ in range(level_difference // 2):
+                    ns.current_frame = ns.current_frame.parentFrame
+                
+                ns.current_level = level
+            
+            call = Call(node)
+            ns.current_frame.add_call(call)
+            
+        
+    traverse_AST_level(ast, create_frames_helper)
     return ns.frame
 
 def traverse_frames(frame, fn):
@@ -334,16 +428,26 @@ def alpha_rename(frame):
     # Phase 1: Rename the left operands of assignment statements
     #          containing the var keyword.
     def rename_identifier_var(frame):
+        global SEPARATOR
         if type(frame) == Frame:
             for var in frame.varDecls:
-                new_identifier = var.identifier + "_" + str(frame.id)
+                new_identifier = var.identifier + SEPARATOR + str(frame.id)
                 debugprint("Renaming variable " + var.identifier + " to " + new_identifier)
                 var.set_identifier(new_identifier)
     
     debugprint("Phase 1")
     traverse_frames(frame, rename_identifier_var)
     
-    # Phase 2: Rename the right operands of assignment statements
+    # Phase 2: Rename all functions
+    def rename_function(frame):
+        global FUNCSEP
+        if type(frame) == Frame:
+            new_name = frame.name + FUNCSEP + str(frame.id)
+            frame.set_name(new_name)
+    
+    traverse_frames(frame, rename_function)
+    
+    # Phase 3: Rename the right operands of assignment statements
     #          containing the var keyword
     def evaluate_operand_var(frame):
         if type(frame) == Frame:
@@ -358,12 +462,24 @@ def alpha_rename(frame):
                 new_value = frame.lookup_variable(value)
                 debugprint("Lookup variable successful. {0}".format(new_value))
                 var.set_value(new_value if new_value is not None else "unknown")
-                
-    debugprint("Phase 2")
+    
+    debugprint("Phase 3")
     traverse_frames(frame, evaluate_operand_var)
     
-    # Phase 3: Rename the left operands of assignment statements
-    #          without the var keyword.
+    # Phase 4: Rename calls
+    def rename_calls(frame):
+        if type(frame) == Frame:
+            for call in frame.callList:
+                name = call.name
+                
+                # TODO: Might want to add isprimitive
+                
+                new_name = frame.lookup_function(name)
+                call.set_name(new_name if new_name is not None else "unknown")
+    debugprint("Phase 4")
+    traverse_frames(frame, rename_calls)
+    
+    # Phase 5: Rename unrenamed identifiers
                 
     return frame
                 
@@ -387,7 +503,10 @@ def isprimitive(token):
     >>> isprimitive('lambda')
     False
     """
-    if token in ['true', 'false', "__fparam__", "undefined"]:
+    primitive_list = ['true', 'false',
+                      '__fparam__', 'undefined',
+                      'document', 'console']
+    if token in primitive_list:
         return True
     try:
         float(token)
@@ -405,7 +524,7 @@ def run(*args):
     debugprint()
     debugprint("Rename successful.")
     debugprint()
-#    print(fr)
+    print(fr)
     ast_out.write(str(ast))
     
     
