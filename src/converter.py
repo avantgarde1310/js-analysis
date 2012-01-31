e3e9f7131ed3820d4159ef036bfcaf309ffe8af8
gozaliivan <gozaliivan@7f3d32f9-e71d-42ef-87b3-a642ebc53577>
2012-01-31 07:26:30 +0000

    A simple converter. It converts log files of this format: Extension Name: Google Translate Extension Identifier: aapbdbdomjkkjkaonfhkkikfgjllcleb Number of functions: 96 Number of anonymous functions: 50 Number of named functions: 46 Number of functions that reference chrome: 3
    to this format:
    Google Translate & 96 & 3 & 3.1 \\ \hline
    
    git-svn-id: https://bullseye.cs.berkeley.edu/i-svn/devdatta/2011/extensions/ivan/ExtensionAnalyzer@469 7f3d32f9-e71d-42ef-87b3-a642ebc53577

---
+++ src/converter.py
@@ -0,0 +1,60 @@
+'''
+Created on Jan 30, 2012
+
+@author: Ivan Gozali
+'''
+
+from ucb import main as _main
+import os
+import fileutils
+import argparse
+
+def extract_data_from_text(text):
+    """
+    text - a string in the format below:
+    
+    Extension Name: Clip to Evernote
+    Extension Identifier: pioclpoplcdbaefihamjohnefbikjilc
+    Number of functions: 4241
+    Number of anonymous functions: 4009
+    Number of named functions: 232
+    Number of functions that reference chrome: 170
+    """
+    def get_text_after_colon(str):
+        return str[str.index(":") + 1:]
+    
+    data = text.split("\n")
+    ext_name = get_text_after_colon(data[0]).strip()
+    total_functions = get_text_after_colon(data[2]).strip()
+    chrome_refs = get_text_after_colon(data[5]).strip()
+    percentage_refs = float(chrome_refs) / float(total_functions) * 100
+    
+    # return a string of the format "Google Translate & 87 & 2 & 2.3 \\ \hline"
+    return ext_name + " & " + total_functions + " & " + chrome_refs + " & " + \
+        "{:.1f}".format(percentage_refs) + " \\\\ \\hline"
+        
+
+def extract_data(filelist):
+    str = ""
+    for txtpath in filelist:
+        text = open(txtpath).read()
+        str += extract_data_from_text(text) + "\n"
+    return str
+
+@_main
+def main(*args):
+    parser = argparse.ArgumentParser(description="Module to convert log files into Excel readable format.")
+    
+    parser.add_argument("path", action="store", help="a file to be read in current directory or a directory containing files to be read")
+    parser.add_argument("-o", action="store", dest="outputfile", default="outputfile.txt", help="specifies output file name (default: outputfile.txt)")
+    
+    argobj = parser.parse_args()
+    
+    if os.path.isdir(argobj.path):
+        filelist = fileutils.get_all_files_in_directory(argobj.path)
+    if os.path.isfile(os.getcwd() + "\\" + argobj.path):
+        filelist = [os.getcwd() + "\\" + argobj.path]
+    
+    outputfile = open(argobj.outputfile, "w")
+    output_str = extract_data(filelist)
+    outputfile.write(output_str)
